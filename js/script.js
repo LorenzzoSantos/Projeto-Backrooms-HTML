d0b9671455338a09469ba371a7aab2f6feb202fc
LORENZZO GABRIEL ALMEIDA DOS SANTOS <lorenzzo_santos@estudante.sesisenai.org.br>
2026-08-20 15:43:42 -0300

    Algumas modificações pra deixar o site bonitinho

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ const usuariosValidos = {
 
 var loginForm = document.querySelector('#id01 form');
 
-loginForm.addEventListener('submit', function(event){
+loginForm.addEventListener('submit', function login (event){
     event.preventDefault();
 
     var usuario = loginForm.uname.value.trim();
@@ -30,9 +30,38 @@ loginForm.addEventListener('submit', function(event){
         loginForm.reset();
 
         document.getElementById("secreto").style.display = "block";
-
+        document.getElementById("tela-inicial").style.display = "none"
+        document.body.classList.add('fundo-logado');
     }else{alert("usuario incorreto :(");
 
     }
      
+});
+
+const caixa = document.getElementById('secreto');
+
+let arrastando = false;
+let offsetX = 0;
+let offsetY = 0;
+
+// 1. Quando clica na caixa (Segurar)
+caixa.addEventListener('mousedown', (e) => {
+  arrastando = true;
+  // Calcula a distância do clique em relação ao canto da caixa
+  offsetX = e.clientX - caixa.offsetLeft;
+  offsetY = e.clientY - caixa.offsetTop;
+});
+
+// 2. Quando mexe o mouse pela página (Arrastar)
+document.addEventListener('mousemove', (e) => {
+  if (!arrastando) return;
+  
+  // Atualiza a posição da caixa na tela
+  caixa.style.left = `${e.clientX - offsetX}px`;
+  caixa.style.top = `${e.clientY - offsetY}px`;
+});
+
+// 3. Quando solta o botão do mouse (Soltar)
+document.addEventListener('mouseup', () => {
+  arrastando = false;
 });
